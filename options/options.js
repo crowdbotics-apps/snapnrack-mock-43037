1322d4107fdcea8f256d9c1192aacee57f62754d
Crowdbotics <team@crowdbotics.com>
2023-08-23 16:55:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snapnrack-mock-43037",
+  url: "https://snapnrack-mock-43037.botics.co",
+  api: "https://snapnrack-mock-43037.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
