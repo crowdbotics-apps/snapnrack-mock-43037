2f427e45c5bbb37d49d1e2a20a97fc65aad0c285
Jason Kozel <jason.kozel@crowdbotics.com>
2023-08-23 13:37:01 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled6/index.js
+++ screens/Untitled6/index.js
@@ -41,6 +41,24 @@ const Untitled6 = () => {
                 </View>
               </View>
             </View>
+            <View style={styles.section}>
+              <Text style={styles.h2}>Project Info:</Text>
+              <View style={styles.separator} />
+              <View style={styles.colCon}>
+                <View style={styles.col1Sub}>
+                  <Text style={styles.field}>Company</Text>
+                  <Text style={styles.field}>Project Name</Text>
+                </View>
+                <View style={styles.col2Sub}>
+                  <View style={styles.padding}>
+                    <TextInput style={styles.textInput}></TextInput>
+                  </View>
+                  <View style={styles.padding}>
+                    <TextInput style={styles.textInput}></TextInput>
+                  </View>
+                </View>
+              </View>
+            </View>
           </View>
           <View style={styles.col2}></View>
         </View>
@@ -103,7 +121,7 @@ const styles = StyleSheet.create({
     width: "100%"
   },
   padding: {
-    marginTop: 10
+    marginTop: 8
   },
   safeArea: {
     height: "100%"
